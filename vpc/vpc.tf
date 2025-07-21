f39976d4418f74230b017e92fca078aa2ba104a4
moiz01 <moiz.cooltech@gmail.com>
2025-07-21 23:03:15 +0530

    annotation added

---
+++ vpc/vpc.tf
@@ -0,0 +1,25 @@
+
+######
+# VPC
+######
+# Create VPC
+resource "aws_vpc" "base" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}",
+    "kubernetes.io/cluster/eks-cluster-prod" = "shared"
+    }
+  )
+
+}
+
+
+
+
+
+
+
+
